e0a050a4ff43b803d43c14d95bfe09923ae53f30
Yagnnoz <jensk37@gmail.com>
2022-11-03 14:05:47 +0100

    added test to solar calculator

--- src/app/solar-calculator/solar-calculator.component.spec.ts
+++ src/app/solar-calculator/solar-calculator.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SolarCalculatorComponent } from './solar-calculator.component';
+import {Vanilla} from "../mock-solarpanels";
+import {ACC_VANILLA} from "../mock-accumulators";
 
 describe('SolarCalculatorComponent', () => {
   let component: SolarCalculatorComponent;
@@ -20,4 +22,16 @@ describe('SolarCalculatorComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should return the correct values for 20 MW Vanilla (478 Panels & 402 Accumulators)', async () => {
+    component.selectedPanel = Vanilla[0];
+    component.selectedAccumulator = ACC_VANILLA[0];
+    component.powerNeeded=20;
+
+    component.calculate();
+
+    expect(component.result).toBe(true);
+    expect(component.neededSolarPanels).toBe(478);
+    expect(component.neededAccumulators).toBe(402);
+  })
 });
