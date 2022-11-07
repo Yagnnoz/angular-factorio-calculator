483b3970cd09f9961e8bb9872b22e4926ae04aaa
Yagnnoz <jensk37@gmail.com>
2022-11-07 14:11:10 +0100

    main and nuclear components started

---
+++ src/app/nuclear-calculator/nuclear-calculator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nuclear-calculator',
+  templateUrl: './nuclear-calculator.component.html',
+  styleUrls: ['./nuclear-calculator.component.css']
+})
+export class NuclearCalculatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
